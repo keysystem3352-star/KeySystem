61915c64ae5d666cdcfdf49ee957b806ae6d860a
keysystem3352-star <keysystem3352@gmail.com>
2025-09-22 18:14:06 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const ServiceKey = "44pk-uopl-cVIp-kayv-pQjd-QdG1-Dns1-adO0-russa-1ov3r";
 const HashCode_Database = "https://hash-code-20ecd-default-rtdb.firebaseio.com/";
 const HashCode_SavedData = "https://raw.githubusercontent.com/MainScripts352/Database/refs/heads/main/Hash%20Code%20Database";
-const SYSTEM_KEY = "jamx-wpf4-20gn-920g-Il0v3-Russia-382g";
+const SYSTEM_KEY = "02ks-30nd-kanc-lwn5-Il0v3-Russia-382g";
 
 //-- Encode Decode Word Function
 const base32Alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
@@ -94,13 +94,10 @@ function decodeWithSystemKey(hexstr) {
 }
 
 // Get timestamp with days expiration
-async function getTimestamp(days = 0) {
-  const response = await fetch("http://worldclockapi.com/api/json/utc/now");
-  const data = await response.json();
-  const time = data.currentFileTime;
-  const timeMs = Math.floor(time / 10000);
-  const addedMs = days * 24 * 60 * 60 * 1000;
-  return timeMs + addedMs;
+function getTimestamp(days = 0) {
+  const now = Date.now();
+  const add = days * 24 * 60 * 60 * 1000;
+  return now + add;
 }
 
 
@@ -235,6 +232,14 @@ export default {
       let key = path[1];
       key = key.replace("KEY_", "");
       return new Response(decodeWithSystemKey(key), {
+        headers: { "Content-Type": "text/plain" }
+      });
+    }
+
+    // Get Time
+    if (path[0] === "time" && method === "GET") {
+      const time = getTimestamp();
+      return new Response(String(time), {
         headers: { "Content-Type": "text/plain" }
       });
     }
